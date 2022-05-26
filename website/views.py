f6160d07182692c671266549fe09549191adbd36
iliasdodoros <el19206@mail.ntua.gr>
2022-05-27 02:12:13 +0300

    dane

--- website/views.py
+++ website/views.py
@@ -1,18 +1,27 @@
+from datetime import date
 from flask import Blueprint, render_template
 import mysql.connector
 
-ourdb= mysql.connector.connect(host="localhost",user="root",password="",database="elidek")
+ourdb = mysql.connector.connect(
+    host="localhost", user="root", password="", database="elidek")
 
 
-mycursor=ourdb.cursor()
-mycursor.execute("show tables")
+mycursor = ourdb.cursor()
+data= mycursor.execute("SELECT * FROM Programm")
+mytable= mycursor.fetchall()
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
     return render_template("home.html")
 
+
 @views.route('/Organismos')
 def Organismos():
     return render_template("Organismos.html")
+
+@views.route('/Stelehos')
+def Stelehos():
+    return render_template("Stelehos.html",data=mytable)
