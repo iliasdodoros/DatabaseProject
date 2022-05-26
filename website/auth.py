d32d6f505c27186202902943cd080c94c488b310
eimaiokitsis <theo.kitsis.2001@gmail.com>
2022-05-26 20:32:19 +0300

    u

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,25 @@ from flask import Blueprint
 
 auth = Blueprint('auth' , __name__)
 
+@auth.route('/Program')
+def Programs():
+        return "<p>Programs</p>"
 
+@auth.route('/Project')
+def Project():
+        return "<p>Project</p>"
 
+@auth.route('/Science_Field')
+def Science_Field():
+        return "<p>Science_Field</p>"
 
+@auth.route('/Organismos')
+def Organismos():
+        return "<p>Organismos</p>"
+
+@auth.route('/Stelehos')
+def Stelehos():
+        return "<p>Stelehos</p>"
+@auth.route('/Researcher')
+def Researcher():
+        return "<p>Researcher</p>" 
