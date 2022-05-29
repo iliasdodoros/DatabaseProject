31b761e2274a0d97f0b47a65e0f2c08796f1e86f
iliachilia <liana.xatz@gmail.com>
2022-05-29 21:02:37 +0300

    Update query3.sql

--- db/query3.sql
+++ db/query3.sql
@@ -1,16 +1,18 @@
-(select p.title, r.last_name, r.name  
+use elidek;
+
+(select p.title, r.last_name, r.first_name  
 from researcher r 
 inner join works_in_project wip on r.researcher_id = wip.researcher_id 
 inner join project p on wip.project_id = p.project_id 
 inner join project_research_field prf on p.project_id = prf.project_id
-where (prf.name =  )
-and (p.beginning < (SELECT CAST( GETDATE() AS Date )) and p.ending > (SELECT CAST( GETDATE() AS Date ))))
+where (prf.name = 'History')
+and ((p.beginning < current_date())  and (p.ending > current_date()))
 union 
-(select p.title, r.last_name, r.name  
+(select p.title, r.last_name, r.first_name  
 from researcher r 
 inner join project p2 on r.researcher_id = p2.supervisor_id 
 inner join project_research_field prf on p.project_id = prf.project_id 
-where (prf.name =  )
-and (p.beginning < (SELECT CAST( GETDATE() AS Date )) and p.ending > (SELECT CAST( GETDATE() AS Date ))));
+where (prf.name = 'History')
+and ((p.beginning < current_date())  and (p.ending > current_date()));
 
 
