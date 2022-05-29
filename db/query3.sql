f93db2d88d7d72134bca69ffc0ca2be478e18188
iliachilia <liana.xatz@gmail.com>
2022-05-29 21:09:55 +0300

    parto

--- db/query3.sql
+++ db/query3.sql
@@ -1,18 +1,18 @@
 use elidek;
 
 (select p.title, r.last_name, r.first_name  
-from researcher r 
-inner join works_in_project wip on r.researcher_id = wip.researcher_id 
-inner join project p on wip.project_id = p.project_id 
-inner join project_research_field prf on p.project_id = prf.project_id
+from Researcher r 
+inner join Works_in_Project wip on r.researcher_id = wip.researcher_id 
+inner join Project p on wip.project_id = p.project_id 
+inner join Project_Research_Field prf on p.project_id = prf.project_id
 where (prf.name = 'History')
-and ((p.beginning < current_date())  and (p.ending > current_date()))
+and ((p.beginning < current_date())  and (p.ending > current_date())))
 union 
 (select p.title, r.last_name, r.first_name  
-from researcher r 
-inner join project p2 on r.researcher_id = p2.supervisor_id 
-inner join project_research_field prf on p.project_id = prf.project_id 
+from Researcher r 
+inner join Project p on r.researcher_id = p.supervisor_id 
+inner join Project_Research_Field prf on p.project_id = prf.project_id 
 where (prf.name = 'History')
-and ((p.beginning < current_date())  and (p.ending > current_date()));
+and ((p.beginning < current_date())  and (p.ending > current_date())));
 
 
