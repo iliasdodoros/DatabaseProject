bc7e78a5ba30fc1c130573b2398be60a148b1b42
iliasdodoros <el19206@mail.ntua.gr>
2022-05-29 20:13:20 +0300

    fe

--- dbfile.sql
+++ dbfile.sql
@@ -451,15 +451,6 @@ VALUES
 	('female','Aggeliou','1974-01-04','Aggeliki','100','10');
 	
 
-INSERT into Project
-	(`amount`,`title`,`beginning`,`ending`,`duration`,`summary`,`project_id`,`grade`,`date_of_grading`,`stelehos_id`,`programm_id`,`supervisor_id`,`grader_id`,`organisation_id`) 
-VALUES 
-	('200.000','Mopping the sea','01-01-2020','01-01-2023','3','lorem ipsum','1','1','1','1','1','1'),
-	
--- INSERT into Delivered
--- 	(`title`,`summary`,`delivered_id`,`project_id`) 
--- VALUES 
-
 INSERT into Project_research_field 
 	(`project_id`,`name`) 
 VALUES 
