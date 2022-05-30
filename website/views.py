a77075cbe66efaefd836fcba128ea62c3c690742
iliasdodoros <el19206@mail.ntua.gr>
2022-05-30 16:29:35 +0300

    hgfh

--- website/views.py
+++ website/views.py
@@ -7,9 +7,8 @@ ourdb = mysql.connector.connect(
     host="localhost", user="root", password="", database="elidek")
 
 
-mycursor = ourdb.cursor()
-datau = mycursor.execute("SELECT * FROM Programm")
-mytable = mycursor.fetchall()
+mycursor1 = ourdb.cursor()
+mycursor2 = ourdb.cursor()
 
 views = Blueprint('views', __name__)
 
@@ -24,8 +23,8 @@ def Organismos():
     if request.method == 'POST':
         data = request.form["i"]
         if data:
-            lost = mycursor.execute(f'SELECT * FROM {data}')
-            table = mycursor.fetchall()
+            lost = mycursor1.execute(f'SELECT * FROM {data}')
+            table = mycursor1.fetchall()
             listed = list(table)
             return render_template("Organismos.html", table=listed, rows=len(table), columns=len(table[0]), boolean=True)
     else:
@@ -40,22 +39,22 @@ def Stelehos():
 
 @views.route('/Programm')
 def Researcher():
-    found = mycursor.execute('SELECT * FROM Stelehos')
-    researchers = mycursor.fetchall()
-    vlaka = mycursor.execute('SELECT * FROM Research_Field')
-    fields = mycursor.fetchall()
+    found = mycursor1.execute('SELECT * FROM Stelehos')
+    researchers = mycursor1.fetchall()
+    vlaka = mycursor1.execute('SELECT * FROM Research_Field')
+    fields = mycursor1.fetchall()
     return render_template("Programm.html", researchers=researchers, fields=fields)
 
 
 @views.route('/Research_Field', methods=['GET', 'POST'])
 def Research_Field():
-    lost = mycursor.execute(f'SELECT * FROM Research_Field')
-    fields = mycursor.fetchall()
-    listed = list(fields)
+
+    lost = mycursor1.execute(f'SELECT * FROM Research_Field')
+    fields = mycursor1.fetchall()
     if request.method == 'POST':
         choice = request.form["resfield"]
         if choice:
-            lost = mycursor.execute(f'''(select p.title, r.last_name, r.first_name  
+            lost = mycursor2.execute(f'''(select p.title, r.last_name, r.first_name  
                                     from Researcher r 
                                     inner join Works_in_Project wip on r.researcher_id = wip.researcher_id 
                                     inner join Project p on wip.project_id = p.project_id 
@@ -68,9 +67,8 @@ def Research_Field():
                                     inner join Project p on r.researcher_id = p.supervisor_id 
                                     inner join Project_Research_Field prf on p.project_id = prf.project_id 
                                     where (prf.name = '{choice}')
-                                    and ((p.beginning < current_date())  and (p.ending > current_date())));''',multi=True)
-            results=mycursor.fetchall()
-            return render_template("Research_Field.html", fields=listed)
+                                    and ((p.beginning < current_date())  and (p.ending > current_date())));''', multi=True)
+            results = mycursor2.fetchall()
+            return render_template("Research_Field.html", fields=fields, results=results,rows=len(results), columns=len(results[0]), boolean=True)
 
-    else:
-         return render_template("Research_Field.html",fields=fields)
+    return render_template("Research_Field.html", fields=fields)
