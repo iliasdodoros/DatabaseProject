ef7b289140cf847f1c4055d35db8e22c13d12439
iliachilia <liana.xatz@gmail.com>
2022-05-31 21:33:46 +0300

    Update query5.sql

--- db/query5.sql
+++ db/query5.sql
@@ -1,1 +1,15 @@
-select Research_Field, Research_Field1, count() from pair_of_fields group by Research_Field, Research_Field1 order by count() desc limit 3;
+create view project_and_rf as
+select p.title, p.project_id, prf.name  
+from project p 
+inner join project_research_field prf on p.project_id = prf.project_id 
+order by p.project_id ;
+
+select title, project_id, rf_duo, count(*) as counter from (
+select prf1.title, prf1.project_id, concat (prf1.name," ", prf2.name) rf_duo
+from project_and_rf prf1 
+inner join project_and_rf prf2 on prf1.title = prf2.title
+where prf1.name != prf2.name and prf1.name < prf2.name) A 
+group by rf_duo
+order by counter desc
+limit 3;
+
