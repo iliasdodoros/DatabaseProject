4a5c8eeab3d7838fc55e76b5cd63622a5fdd7346
iliachilia <liana.xatz@gmail.com>
2022-05-31 20:16:16 +0300

    Update query8.sql

--- db/query8.sql
+++ db/query8.sql
@@ -1,1 +1,17 @@
-select * from res_proj_no_del where projects_working_on >= 5;
+select * from (
+(select r.last_name, r.first_name, count(*) as projects_working_on
+from researcher r 
+inner join works_in_project wip on r.researcher_id = wip.researcher_id 
+inner join project p on wip.project_id = p.project_id 
+left join delivered d on p.project_id = d.project_id 
+where d.title is null 
+group by r.last_name)
+union
+(select r.last_name, r.first_name, count(*) as projects_working_on 
+from researcher r 
+inner join project p on r.researcher_id  = p.supervisor_id
+left join delivered d on p.project_id = d.project_id 
+where d.title is null 
+group by r.last_name ) 
+order by projects_working_on desc) A
+where projects_working_on >= 5;
