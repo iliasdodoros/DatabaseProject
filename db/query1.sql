8e96daf11652a44f130c07467cab9cf50f161ff2
iliachilia <liana.xatz@gmail.com>
2022-06-02 15:51:17 +0300

    Update query1.sql

--- db/query1.sql
+++ db/query1.sql
@@ -13,3 +13,19 @@ inner join Project p on r.researcher_id = p.supervisor_id
 order by title_of_project)) A
 where A.duration = 24
 order by title_of_project ;
+------------------------------------------------------------------------
+select title_of_project , name_of_researcher  from (
+(select concat(r.last_name," ", r.first_name) name_of_researcher, p.title as title_of_project,  s.name 
+from Researcher r 
+inner join Works_in_Project wip on r.researcher_id = wip.researcher_id 
+inner join Project p on wip.project_id = p.project_id
+inner join Stelehos s on p.stelehos_id = s.stelehos_id 
+order by title_of_project )
+union 
+(select concat(r.last_name," ", r.first_name) name_of_researcher, p.title as title_of_project, s.name  
+from Researcher r 
+inner join Project p on r.researcher_id = p.supervisor_id 
+inner join Stelehos s on p.stelehos_id = s.stelehos_id 
+order by title_of_project)) A   
+where A.name = "Johnny Depp";
+order by title_of_project ;
