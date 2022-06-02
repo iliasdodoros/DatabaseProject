593bba094ec3d522982fa424a7415544632a5b0d
theo.kitsis.2001 <theo.kitsis.2001@ygmail.com>
2022-06-02 18:25:50 +0300

    j

--- website/views.py
+++ website/views.py
@@ -72,22 +72,22 @@ def Researchers_under_40():
 @views.route('/Researcher/Researchers_working_on')
 def Researchers_working_on():
     mycursor.execute('''select * from (
-                        (select r.last_name, r.first_name, count(*) as projects_working_on
-                        from Researcher r 
-                        inner join Works_in_Project wip on r.researcher_id = wip.researcher_id 
-                        inner join Project p on wip.project_id = p.project_id 
-                        left join Delivered d on p.project_id = d.project_id 
-                        where d.title is null 
-                        group by r.last_name)
-                        union
-                        (select r.last_name, r.first_name, count(*) as projects_working_on 
-                        from Researcher r 
-                        inner join Project p on r.researcher_id  = p.supervisor_id
-                        left join Delivered d on p.project_id = d.project_id 
-                        where d.title is null 
-                        group by r.last_name ) 
-                        order by projects_working_on desc) A
-                        where projects_working_on >= 5;''')
+                        select concat(last_name, " ", first_name) as name_of_researcher, count(*) as projects_working_on  from (
+                        (select r.last_name, r.first_name
+                        from researcher r 
+                        inner join works_in_project wip on r.researcher_id = wip.researcher_id 
+                        inner join project p on wip.project_id = p.project_id 
+                        left join delivered d on p.project_id = d.project_id 
+                        where d.title is null )
+                        union all
+                        (select r.last_name, r.first_name
+                        from researcher r 
+                        inner join project p on r.researcher_id  = p.supervisor_id
+                        left join delivered d on p.project_id = d.project_id 
+                        where d.title is null  ) ) A
+                        group by A.last_name ) B
+                        where projects_working_on >= 5
+                        order by projects_working_on desc;''')
     result6 = mycursor.fetchall()
     listed6 = list(result6)
     return render_template("Researchers_working_on.html", result6=listed6, rows=len(result6), columns=len(result6[0]), boolean=True)
