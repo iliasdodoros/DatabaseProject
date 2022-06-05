bf23107cb51cb57b1294f74490bcf154ac57d966
Chatzilia Iliana <liana.xatz@gmail.com>
2022-06-05 22:53:59 +0300

    $

--- db/create_db.sql
+++ db/create_db.sql
@@ -90,7 +90,7 @@ CREATE TABLE Researcher
   PRIMARY KEY (researcher_id),
   KEY idx_fk_organisation_id (organisation_id),
   KEY idx_date_of_birth (date_of_birth),
-  CONSTRAINT `fk_researcher_organisation` FOREIGN KEY (organisation_id) REFERENCES Organisation(organisation_id) ON DELETE set NULL ON UPDATE CASCADE
+  CONSTRAINT `fk_researcher_organisation` FOREIGN KEY (organisation_id) REFERENCES Organisation(organisation_id) ON DELETE cascade ON UPDATE CASCADE
 );
 
 CREATE TABLE Project
@@ -119,11 +119,11 @@ CREATE TABLE Project
   KEY idx_beginning (beginning),
   KEY idx_ending (ending),
   KEY idx_duration (duration),
-  CONSTRAINT `fk_project_stelehos` FOREIGN KEY (stelehos_id) REFERENCES Stelehos(stelehos_id) ON DELETE set NULL ON UPDATE cascade,
-  CONSTRAINT `fk_project_programm` FOREIGN KEY (programm_id) REFERENCES Programm(programm_id) ON DELETE set NULL ON UPDATE cascade,
-  CONSTRAINT `fk_project_supervisor` FOREIGN KEY (supervisor_id) REFERENCES Researcher(researcher_id) ON DELETE set NULL ON UPDATE cascade,
-  CONSTRAINT `fk_project_grader` FOREIGN KEY (grader_id) REFERENCES Researcher(researcher_id) ON DELETE set NULL ON UPDATE cascade,
-  CONSTRAINT `fk_project_organisation` FOREIGN KEY (organisation_id) REFERENCES Organisation(organisation_id) ON DELETE set NULL ON UPDATE CASCADE
+  CONSTRAINT `fk_project_stelehos` FOREIGN KEY (stelehos_id) REFERENCES Stelehos(stelehos_id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT `fk_project_programm` FOREIGN KEY (programm_id) REFERENCES Programm(programm_id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT `fk_project_supervisor` FOREIGN KEY (supervisor_id) REFERENCES Researcher(researcher_id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT `fk_project_grader` FOREIGN KEY (grader_id) REFERENCES Researcher(researcher_id) ON DELETE cascade ON UPDATE cascade,
+  CONSTRAINT `fk_project_organisation` FOREIGN KEY (organisation_id) REFERENCES Organisation(organisation_id) ON DELETE cascade ON UPDATE CASCADE
 );
 
 alter table Project add constraint check(amount between 100000 and 1000000);
