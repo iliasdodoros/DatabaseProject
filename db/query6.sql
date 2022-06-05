bcc1e473d48535424c1966f5b9d90f932a16d823
iliachilia <liana.xatz@gmail.com>
2022-06-05 03:26:03 +0300

    100/100

--- db/query6.sql
+++ db/query6.sql
@@ -1,22 +1,16 @@
-
-create view active_projects as
-	select * from Project p 
-	where ((p.beginning < current_date())  and (p.ending > current_date()));	
-
-
-(select r.last_name, r.first_name, count(*) as projects_working_on 
-from Researcher r 
-inner join Works_in_Project wip on r.researcher_id = wip.researcher_id 
+select last_name, first_name, count(*) as projects_working_on from (
+(select r.last_name, r.first_name 
+from researcher r 
+inner join works_in_project wip on r.researcher_id = wip.researcher_id 
 inner join active_projects ap on wip.project_id = ap.project_id 
-where r.date_of_birth > '1981-12-31'
-group by r.last_name)
-union 
-(select r.last_name, r.first_name, count(*) as projects_working_on 
-from Researcher r 
+where r.date_of_birth > '1981-12-31')
+union all
+(select r.last_name, r.first_name
+from researcher r 
 inner join active_projects ap on r.researcher_id  = ap.supervisor_id
-where r.date_of_birth > '1981-12-31' 
-group by r.last_name ) 
+where r.date_of_birth > '1981-12-31') ) A
+group by last_name 
 order by projects_working_on desc ;
 
 
-drop view active_projects;
+
